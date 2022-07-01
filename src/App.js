9c34e19e3e34837a980c28fc10a8ddb944284669
CodeMasterBrew <rajesh.raushan.1999@gmail.com>
2022-07-01 19:33:58 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
-// import Homepage from './components/Homepage';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-// import PlayVideo from './components/PlayVideo';
+import Homepage from './components/Homepage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import PlayVideo from './components/PlayVideo';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      {/* <Router>
+      <Header />
+      <Router>
         <Routes>
+          
           <Route exact path="/" element={<Homepage/>} />
           <Route exact path="/playvideo" element={<PlayVideo/>} />
         </Routes>
-      </Router> */}
-      <Header />
+      </Router>
+      
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,8 @@ export default function Header() {
         <div className='header-container'>
             <p className='commments-header'>☰</p>
             <img height="60" width="150" src={logo} />
-            {/* <input type='text' style={{padding: "5px", margin: "20px",height:"60px",width:"511px",}} placeholder= "search" /> */}
             <input type="text" placeholder="Search.." name="search" />
-            <button type="submit"><i class="fa fa-search"></i></button>
+            <button type="submit"><i className="fa fa-search"></i></button>
         </div>
     )
 }
